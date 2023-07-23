625ec7d805fbd91265cd2fdff4c77f9ee0494e68
Luis Arteaga <arteaga@live.de>
2023-07-23 14:23:48 +0000

    outsource logic to config.py

--- main.py
+++ main.py
@@ -1,43 +1,42 @@
 import os
 import requests
-from azure.keyvault.secrets import SecretClient
-from azure.identity import ClientSecretCredential
-from azure.storage.blob import BlobServiceClient
+import config
 
-AZURE_TENANT_ID = os.environ['AZURE_TENANT_ID']
-SERVICE_PRINCIPAL_AZURE_APP_ID = os.environ['SERVICE_PRINCIPAL_AZURE_APP_ID']
-SERVICE_PRINCIPAL_AZURE_APP_SECRET = os.environ['SERVICE_PRINCIPAL_AZURE_APP_SECRET']
-AZURE_KEY_VAULT_URL = os.environ['AZURE_KEY_VAULT_URL']
+AzureServicesConnector = config.AzureServicesConnector()
+
 AZURE_KEY_VAULT_API_NAME_NASA_GOV = os.environ['AZURE_KEY_VAULT_API_NAME_NASA_GOV']
-AZURE_STORAGE_ACCOUNT_URL = os.environ['AZURE_STORAGE_ACCOUNT_URL']
+AZURE_STORAGE_CONTAINER_NAME = os.environ['AZURE_STORAGE_CONTAINER_NAME']
 
-credential = ClientSecretCredential(
-    tenant_id=AZURE_TENANT_ID,
-    client_id=SERVICE_PRINCIPAL_AZURE_APP_ID,
-    client_secret=SERVICE_PRINCIPAL_AZURE_APP_SECRET
-)
-
-secret_client = SecretClient(vault_url=AZURE_KEY_VAULT_URL, credential=credential)
-secret = secret_client.get_secret(AZURE_KEY_VAULT_API_NAME_NASA_GOV)
-
-NASA_API_KEY = secret.value
+nasa_api_key = AzureServicesConnector.get_secret(AZURE_KEY_VAULT_API_NAME_NASA_GOV)
+storage_container = AzureServicesConnector.get_container_client(AZURE_STORAGE_CONTAINER_NAME)
 
 def retrieve_data_from_nasa_api(api_key, start_date, end_date):
     url = f"https://api.nasa.gov/DONKI/CME?startDate={start_date}&endDate={end_date}&api_key={api_key}"
     response = requests.get(url)
-    data = response.json()
+    data = response
     return data
 
-#sample_data = retrieve_data_from_nasa_api(NASA_API_KEY, "2023-06-23", "2023-07-23")
+sample_data = retrieve_data_from_nasa_api(nasa_api_key, "2023-06-23", "2023-07-23")
 
-# connect to azure storage
+storage_container.upload_blob(name="nasa-gov/donki/coronal-mass-ejection/01-raw/sample_data.json", data=sample_data)
 
-from azure.storage.blob import ContainerClient
 
-service = BlobServiceClient(account_url=AZURE_STORAGE_ACCOUNT_URL, credential=credential)
 
-print(next(service.list_containers()))
 
+#print(next(service.list_containers()))
+
+# Save sample_data in blob nasa-gov
+#container.upload_blob(name="sample_data.json", data=sample_data)
+#print(container.get_blob_client("sample_data.json").url)
+# print(container.get_blob_client("sample_data.json").download_blob().readall())
+# print(container.get_blob_client("sample_data.json").download_blob().content_settings.content_type)
+# print(container.get_blob_client("sample_data.json").download_blob().content_settings.content_encoding)
+# print(container.get_blob_client("sample_data.json").download_blob().content_settings.content_language)
+# print(container.get_blob_client("sample_data.json").download_blob().content_settings.cache_control)
+# print(container.get_blob_client("sample_data.json").download_blob().content_settings.content_disposition)
+# print(container.get_blob_client("sample_data.json").download_blob().content_settings.content_md5)
+# print(container.get_blob_client("sample_data.json").download_blob().content_settings.last_modified)
+# print(container.get_blob_client("sample_data.json").download_blob().metadata)
 
 
 
