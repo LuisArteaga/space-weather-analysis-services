962aec73d94a893d8ffe19ee5edea13b6ad35418
Luis Arteaga <arteaga@live.de>
2023-07-28 20:53:21 +0000

    add get_param_to_date_time_frame function

--- utils.py
+++ utils.py
@@ -1,1 +1,11 @@
+import datetime
 
+def get_param_to_date_time_frame(days: int = 7) -> tuple:
+    ''' 
+    Get the the start and end date of the time frame.
+    :param days: number of days to go back
+    :return: start_date, end_date in string format YYYY-MM-DD 
+    '''
+    end_date = datetime.datetime.now()
+    start_date = end_date - datetime.timedelta(days=days)
+    return start_date.strftime("%Y-%m-%d"), end_date.strftime("%Y-%m-%d")
