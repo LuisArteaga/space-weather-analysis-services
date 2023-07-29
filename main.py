11a031af01ed5114d0d866d35617510af843b5db
Luis Arteaga <arteaga@live.de>
2023-07-29 06:08:48 +0000

    cascading changes

--- main.py
+++ main.py
@@ -10,5 +10,4 @@ storage_container = AzureServicesConnector.get_container_client()
 
 extraction_start_date, extraction_end_date = utils.get_param_to_date_time_frame(7)
 
-extract_cme_status = NasaGovAPI.extract_coronal_mass_ejection(start_date=extraction_start_date, end_date=extraction_end_date, storage_container=storage_container)
-
+extract_cme_status = NasaGovAPI.extract_coronal_mass_ejection(start_date=extraction_start_date, end_date=extraction_end_date, storage_container=storage_container, api_key=nasa_api_key)
