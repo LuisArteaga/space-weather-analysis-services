120d5d0caef21ec6ab5787e90c73d9b0885cc4c8
Luis Arteaga <arteaga@live.de>
2023-07-29 06:37:59 +0000

    add more data extraction points

--- main.py
+++ main.py
@@ -11,3 +11,9 @@ storage_container = AzureServicesConnector.get_container_client()
 extraction_start_date, extraction_end_date = utils.get_param_to_date_time_frame(7)
 
 extract_cme_status = NasaGovAPI.extract_data_from_nasa_api(start_date=extraction_start_date, end_date=extraction_end_date, storage_container=storage_container, api_key=nasa_api_key, api_name='coronal-mass-ejection')
+
+
+extract_flr_status = NasaGovAPI.extract_data_from_nasa_api(start_date=extraction_start_date, end_date=extraction_end_date, storage_container=storage_container, api_key=nasa_api_key, api_name='solar-flare')
+
+
+extract_gst_status = NasaGovAPI.extract_data_from_nasa_api(start_date=extraction_start_date, end_date=extraction_end_date, storage_container=storage_container, api_key=nasa_api_key, api_name='geomagnetic-storm')
